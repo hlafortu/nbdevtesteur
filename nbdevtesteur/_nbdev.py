1beb6ae75283ec0bdc512b6806e26b11c3d5f4b7
hlafortu <hugolafortunebrunet@hotmail.com>
2019-12-28 13:04:23 -0500

    Momo

--- nbdevtesteur/_nbdev.py
+++ nbdevtesteur/_nbdev.py
@@ -3,7 +3,8 @@
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {"say_hello": "00_core.ipynb",
-         "HelloSayer": "00_core.ipynb"}
+         "HelloSayer": "00_core.ipynb",
+         "wrap": "01_coretest.ipynb"}
 
 modules = ["core.py",
            "coretest.py"]
